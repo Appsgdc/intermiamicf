1ae8016761be2dbbbdf81920db306c913dea0bc5
Appsgdc <142878050+Appsgdc@users.noreply.github.com>
2025-09-29 16:12:53 +0530

    Add/Update model for XLE_ENTITY_PROFILES

---
+++ models/Consumption/XLE_ENTITY_PROFILES.sql
@@ -0,0 +1,8 @@
+select *
+from `dev.public.XLE_ENTITY_PROFILES`
+{% if is_incremental() %}
+  where LASTUPDATEDATE >= (
+    select coalesce(max(LASTUPDATEDATE), timestamp('1900-01-01 00:00:00'))
+    from {{ this }}
+  )
+{% endif %}
