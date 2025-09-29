e33af94c27ea43b3c3826895d51eb17cfe6922bd
kirankumar kandra <kiran.kandra@gmail.com>
2025-09-29 10:44:43 +0000

    commit for new

--- models/prestage_1/GL_JE_HEADERS.sql
+++
@@ -1,8 +0,0 @@
-select *
-from `useful-lattice-472005-i4.hr_demo.GL_JE_HEADERS`
-{% if is_incremental() %}
-  where LASTUPDATEDATE >= (
-    select coalesce(max(LASTUPDATEDATE), timestamp('1900-01-01 00:00:00'))
-    from {{ this }}
-  )
-{% endif %}
